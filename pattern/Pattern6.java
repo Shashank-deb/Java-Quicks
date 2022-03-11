acd6ef7bde6b082e642c666efda5d42e91491231
Shashank Sharma <sharmashashank446@gmail.com>
2022-03-11 19:18:35 +0530

    pattern folder is added

---
+++ pattern/Pattern6.java
@@ -0,0 +1,15 @@
+public class Pattern6 
+{
+	public static void main(String[] args) 
+	{
+		int count=1;
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(count++);
+			}
+			System.out.println("");
+		}
+	}
+}
